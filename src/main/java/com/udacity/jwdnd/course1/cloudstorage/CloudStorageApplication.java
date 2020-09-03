e7aa3673bb72e4b26dcbde72e426809ae455fcc1
Rajiv Singh <rs481441@gmail.com>
2020-09-04 02:53:36 +0530

    extends SpringBootServletInitializer in main class

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -3,9 +3,10 @@ package com.udacity.jwdnd.course1.cloudstorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CloudStorageApplication {
+public class CloudStorageApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStorageApplication.class, args);
